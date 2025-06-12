01fc3905a6df675f7bcf9bca8c1bd56a44a680b1
muhammedumar004 <muhammedumar6@gmail.com>
2025-06-12 01:22:23 -0700

    basic js

---
+++ app.js
@@ -0,0 +1,18 @@
+// var username = prompt("What's your name?");
+// alert("Welcome, " + username );
+
+// var username = prompt("What's your name?");
+// var age = prompt("How old are you");
+// console.log(username + " is " + age + " years old");
+
+// var color = prompt("What is your favorite color");
+// alert("Wow " + color + " is a nice color");
+
+// var num1 = prompt("Enter the first number:");
+// var num2 = prompt("Enter the second number:");
+// console.log("The result of " + num1 + " + " + num2 + " is " + (Number(num1) + Number(num2)) );
+
+// var usernamename = prompt("What's your name?");
+// var hobby = prompt("What's your favorite hobby?");
+// var country = prompt("Which country are you from?");
+// alert("Hi, I'm " + username + " from " + country + ", and I love " + hobby );
