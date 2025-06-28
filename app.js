47221c56b0989be087dd3d56fdac227907a2184d
muhammedumar004 <muhammedumar6@gmail.com>
2025-06-28 08:28:51 -0700

    assiment 2

--- app.js
+++ app.js
@@ -16,3 +16,47 @@
 // var hobby = prompt("What's your favorite hobby?");
 // var country = prompt("Which country are you from?");
 // alert("Hi, I am  " + username + " from " + country + ", and I love " + hobby );
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    
+    // var street = prompt("Task 1: What is your street?");
+    // var city = prompt("What is your city?");
+    // var country = prompt("What is your country?");
+    // var fullAddress = "Your full address is: " + street + ", " + city + ", " + country;
+    // alert(fullAddress);
+
+
+    // var firstName = prompt("Task 2: What is your first name?");
+    // var favNumber = prompt("What is your favorite number?");
+    // var username = firstName + favNumber;
+    // alert("Your new username is: " + username);
+
+    // var fName = prompt("Task 3: What is your first name?");
+    // var lName = prompt("What is your last name?");
+    // var email = fName.toLowerCase() + "." + lName.toLowerCase() + "@example.com";
+    // alert("Your email is: " + email);
+
+    
+    // var favColor = prompt("Task 4: What is your favorite color?");
+    // var favAnimal = prompt("What is your favorite animal?");
+    // var favFood = prompt("What is your favorite food?");
+    // var favSentence = "Your favorite things are: " + favColor + " " + favAnimal + "s and " + favFood + "!";
+    // alert(favSentence);
+
+    var quote = prompt("Task 5: Enter your favorite quote:");
+    var author = prompt("Who said it?");
+    alert('"' + quote + '" - ' + author);
